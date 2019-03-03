a8a5780a1a4e037c819817897b2cd48924306df3
Maxs <maxim1998g@gmail.com>
2019-03-04 01:20:29 +0200

    Add UI skeleton

--- lab_1/haskel_db.hs
+++ lab_1/haskel_db.hs
@@ -1,27 +1,3 @@
-{-|
-sign in
-
-  software (+term)
-    add
-    show
-      edit
-      delete
-  
-  author
-    add
-    show
-      edit
-      delete
-    
-  using
-    add
-    show
-      edit
-      delete
-  
-  show info by software   
--}  
-
 {-# LANGUAGE OverloadedStrings #-}
 
 import Database.MySQL.Simple
@@ -29,6 +5,14 @@ import Control.Monad
 import Data.Text as Text
 import Data.Typeable
 --import Data.Text.Format
+import qualified System.Process as SP
+
+--TODO: change before merge
+clearScreen :: IO ()
+clearScreen = do
+  {-_ <- SP.system "reset"
+  return ()-}
+  putStr "\ESC[2J"
 
 data User = User {login :: String, password :: String} deriving (Show)
 
@@ -36,25 +20,99 @@ separator = "*******************************************************************
 
 startProg :: Connection -> IO ()
 startProg conn = do
-  putStrLn (separator ++ "\n\t\t\tSIGN IN\n" ++ separator  )
-  putStrLn "Enter your login: "
+  putStrLn (separator ++ "\n\t\t\tSIGN IN\n" ++ separator)
+  putStrLn "Login: "
   login' <- getLine
-  putStrLn "Enter your password: "
+  putStrLn "Password: "
   password' <- getLine
   putStrLn separator
-  res <- query conn "SELECT * FROM Users where login=? and password=?" (login' :: String, password' :: String)
-  --forM_ res $ \num ->
-    --putStrLn $ Text.unpack fname ++ " " ++ Text.unpack lname
-  --print (res :: [(String, String)]) 
+  res <- query conn "SELECT * FROM Users where login=? and password=?" (login' :: String, password' :: String) 
   case res :: [(String, String)] of
     [] -> do
       putStrLn "ERROR: Please try again"
-      --start conn
+      --TODO: delete before merge
+      menuView conn
     _ -> do
-      putStrLn $ "ALERT: Hello " ++ login'
+      putStrLn $ "Nice to meet you, " ++ login'
+      clearScreen
+      menuView conn
 
---process :: m => m ()
-      
+menuView :: Connection -> IO ()
+menuView conn = do
+  putStrLn "What do you want to change?\n\
+            \\t1. Softwares + its terms\n\
+            \\t2. Authors\n\
+            \\t3. Usage\n\
+            \\t4. Statistic View\n\
+            \_. EXIT"
+  choose <- getLine
+  clearScreen
+  case choose of
+    "1" -> softwareView conn
+    "2" -> authorView conn
+    "3" -> usingView conn
+    "4" -> statisticView conn
+    _ -> putStrLn "Buy!"
+
+softwareView conn = do
+  putStrLn "\tSoftware + Terms:\n\
+            \\t\t1. Edit\n\
+            \\t\t2. Delete\n\
+            \\t\t3. Add\n\
+            \\t\t4. Show\n\
+            \\t_. RETURN"
+  choose <- getLine
+  clearScreen
+  case choose of
+    "1" -> putStrLn "S1!"
+    "2" -> putStrLn "S2!"
+    "3" -> putStrLn "S3!"
+    "4" -> putStrLn "S4!"
+    _ -> menuView conn
+
+authorView conn = do
+  putStrLn "\tAuthors:\n\
+            \\t\t1. Edit\n\
+            \\t\t2. Delete\n\
+            \\t\t3. Add\n\
+            \\t\t4. Show\n\
+            \\t_. RETURN"
+  choose <- getLine
+  clearScreen
+  case choose of
+    "1" -> putStrLn "A1!"
+    "2" -> putStrLn "A2!"
+    "3" -> putStrLn "A3!"
+    "4" -> putStrLn "A4!"
+    _ -> menuView conn
+
+usingView conn = do
+  putStrLn "\tUsing:\n\
+            \\t\t1. Edit\n\
+            \\t\t2. Add\n\
+            \\t\t3. Show\n\
+            \\t_. RETURN"
+  choose <- getLine
+  clearScreen
+  case choose of
+    "1" -> putStrLn "U1!"
+    "2" -> putStrLn "U2!"
+    "3" -> putStrLn "U3!"
+    _ -> menuView conn
+
+statisticView conn = do
+  putStrLn "\tStatistic:\n\
+            \\tFilter by:\n\
+            \\t\t1. Software\n\
+            \\t\t2. Time\n\
+            \\t_. RETURN"
+  choose <- getLine
+  clearScreen
+  case choose of
+    "1" -> putStrLn "SF1!"
+    "2" -> putStrLn "SF2!"
+    _ -> menuView conn
+
 main :: IO () 
 main = do
   conn <- connect defaultConnectInfo
@@ -62,5 +120,6 @@ main = do
     , connectPassword = ""
     , connectDatabase = "haskell"
     }
+  clearScreen
   startProg conn
   close conn
