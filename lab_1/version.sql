038dda8dfba7f171442d1c90acd8669b3b530774
Maxs <maxim1998g@gmail.com>
2019-03-04 03:01:13 +0200

    Update db version, add some default values

--- lab_1/version.sql
+++ lab_1/version.sql
@@ -12,17 +12,16 @@ ENGINE = InnoDB
 CREATE TABLE IF NOT EXISTS `Software` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
-    `terms_id` INT(11) NULL,
     `description` VARCHAR(255),
     `version` VARCHAR(5) NOT NULL,
     `source` VARCHAR(255) NOT NULL DEFAULT 'google.com',
-    PRIMARY KEY (`id`)    
+    PRIMARY KEY (`id`, `name`)    
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Author` (
     `id` INT (11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL UNIQUE,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`, `name`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Software_Author` (
@@ -39,12 +38,11 @@ ENGINE = InnoDB
 ;
 
 CREATE TABLE IF NOT EXISTS `Terms` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
     `software_id` INT(11) NOT NULL,
     `start` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `end` TIMESTAMP NULL,
     `info` VARCHAR(127),
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`software_id`),
     CHECK (`start` < `end`),
     CONSTRAINT `fk_terms_software` FOREIGN KEY (`software_id`)
         REFERENCES `Software` (`id`) ON DELETE CASCADE
@@ -61,10 +59,24 @@ CREATE TABLE IF NOT EXISTS `Using_info` (
         REFERENCES `Software` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
-ALTER TABLE `Software`
-    ADD CONSTRAINT `fk_software_terms` FOREIGN KEY (`terms_id`)
-        REFERENCES `Terms` (`id`) ON DELETE SET NULL;
+/*SOME DEFAULT DATA*/
 
 INSERT `Users`(`login`, `password`)
 VALUES
 ('first', '1');
+
+insert into Author (name)
+values ("author 1"), ("author 2");
+
+insert into Software (name, description, version, source)
+values ("windows", "win desc", "10.1", "computer"),
+("linux", "lin desc", "18.0", "another computer");
+
+insert into Software_Author (software_id, author_id)
+select s.id, a.id from Software as s inner join Author as a on a.name like 'author%' where s.name = "windows";
+
+insert into Using_info (software_id, name, info)
+select id, "student IC-4", "for fun" from Software where name = "windows"; 
+
+insert into Terms (software_id, info)
+select id, "some interesting term" from Software where name = "linux";
