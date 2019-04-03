d52cd7be80807dc4b9f59eba95009edc6f432a26
Maxim <maxim1998g@gmail.com>
2019-04-03 06:50:40 +0300

    Lisp 3: More ideas with gui

--- lab_3/code.lisp
+++ lab_3/code.lisp
@@ -1,17 +1,42 @@
 ; package loader
 (load "~/quicklisp/setup.lisp")
 (ql:quickload "ltk")
+
+(require "ltk")
 ; use gui library
 (in-package #:ltk)
 
-; working test example
-(defun hello-1()
-  (with-ltk ()
-   (let ((b (make-instance 'button 
-                           :master nil
-                           :text "Press Me"
-                           :command (lambda ()
-                                      (format t "Hello World!~&")))))
-     (pack b))))
+;(make-instance 'toplevel)
+;(wm-title *tk* 'KEK)
+;(on-close *tk* (lambda () (progn (format t "CLOSE") (destroy *tk*))))
 
-(hello-1)
+
+(defun hello-2()
+  (with-ltk ()
+   (let* ((f (make-instance 'frame))
+          (varl (list 1 2 3))
+          (b1 (make-instance 'button
+                             :master f
+                             :text "Button 1"
+                             :command (lambda () (format t "Button1~&"))))
+          (b2 (make-instance 'button
+                             :master f
+                             :text "Button 2"
+                             :command (lambda () (format t "Button2~&"))))
+          (l1 (make-instance 'listbox
+                            :master f
+                            :listvariable '()
+                            :command (lambda (x) (print x))
+                           
+                            ))                
+          )
+     (pack f)
+     (pack b1 :side :left)
+     (pack b2 :side :left)
+     (pack l1 :side :left)
+     ;(set l)('ant bee cicada')
+     (configure f :borderwidth 3)
+     (configure f :relief :sunken)
+     )))
+     
+(hello-2)
