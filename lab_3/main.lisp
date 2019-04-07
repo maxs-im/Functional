2001183dd28d8939800c83689fb14fcaa9ae6b06
Maxim <maxim1998g@gmail.com>
2019-04-07 23:26:19 +0300

    Lisp 3: init frame

--- lab_3/main.lisp
+++ lab_3/main.lisp
@@ -7,3 +7,20 @@
 
 ; load my theme classes
 (load (concatenate 'string (directory-namestring *load-pathname*) "classes.lisp"))
+
+; init data
+(read-data-from-file)
+
+(defun gui ()
+    (with-ltk ()
+        (let* ((f (make-instance 'frame))
+            )
+        (wm-title *tk* "Game room")
+
+        (pack f)
+        (configure f :borderwidth 3)
+        (configure f :relief :sunken)
+     
+    )))
+
+(gui)
