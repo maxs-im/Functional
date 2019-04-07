9454c9f80e5ae554935ddc9db24e389cf85f20fc
Maxim <maxim1998g@gmail.com>
2019-04-08 01:05:13 +0300

    Lisp 3: gui - input part

--- lab_3/main.lisp
+++ lab_3/main.lisp
@@ -13,14 +13,36 @@
 
 (defun gui ()
     (with-ltk ()
+        (wm-title *tk* "Game room")
+        ; exit key combination
+        (bind *tk* "<Control-w>" (lambda (event)
+                (declare (ignore event))
+                (setf *exit-mainloop* t)))
         (let* ((f (make-instance 'frame))
-            ; radio select
-            (fradio (make-instance 'frame :master f))
-            (ltoys (make-instance 'label :master fradio :text "Toys: "))
-            (r1 (make-instance 'radio-button :master fradio :text "Ball" :value 'Ball :variable "toy"))
-            (r2 (make-instance 'radio-button :master fradio :text "Machine" :value 'Machine :variable "toy"))
-            (r3 (make-instance 'radio-button :master fradio :text "Constructor" :value 'Construct :variable "toy"))
-            (r4 (make-instance 'radio-button :master fradio :text "Doll" :value 'Doll :variable "toy"))
+            ; input
+            (fparams (make-instance 'frame :master f))
+
+            ; radio toy-type select
+            (fradio (make-instance 'frame :master fparams))
+            (ltoys (make-instance 'label
+                        :master fradio
+                        :text "Type: "))
+            (r1 (make-instance 'radio-button
+                        :master fradio
+                        :text "Ball"
+                        :value 'Ball :variable "toy"))
+            (r2 (make-instance 'radio-button
+                        :master fradio
+                        :text "Machine"
+                        :value 'Machine :variable "toy"))
+            (r3 (make-instance 'radio-button
+                        :master fradio 
+                        :text "Constructor" 
+                        :value 'Construct :variable "toy"))
+            (r4 (make-instance 'radio-button 
+                        :master fradio 
+                        :text "Doll" 
+                        :value 'Doll :variable "toy"))
         
             ; menu for save  
             (mb (make-menubar))
@@ -32,25 +54,80 @@
                                 (save-data-to-file))
                         :underline 1))
             (sepm (add-separator mfile))   
-            (mf-exit (make-menubutton mfile "Exit" (lambda () (setf *exit-mainloop* t))
+            (mf-exit (make-menubutton mfile "Exit" (lambda () 
+                                (setf *exit-mainloop* t))
                         :underline 1
-                        :accelerator "Ctrl Q"))
+                        :accelerator "Ctrl W"))
+        
+            ; Toy name
+            (fname (make-instance 'frame :master fparams))
+            (lname (make-instance 'label
+                        :master fname
+                        :text "Toy name: "))
+            (i-n (make-instance 'entry 
+                        :master fname
+                        :text "name"))
+            ; Toy price borders
+            (fprice (make-instance 'frame :master fparams))
+            (lprice (make-instance 'label
+                        :master fprice
+                        :text "For price (from -> to): "))
+            (i-pf (make-instance 'entry 
+                        :master fprice
+                        :text 0
+                        :validate :key))
+            (i-pt (make-instance 'entry
+                        :master fprice
+	                    :text 0
+	                    :validate :key))
+            ; Toy age borders
+            (fage (make-instance 'frame :master fparams))
+            (lage (make-instance 'label
+                        :master fage
+                        :text "For age (from -> to):   "))
+            (i-af (make-instance 'entry 
+                        :master fage
+                        :text 0
+                        :validate :key))
+            (i-at (make-instance 'entry
+                        :master fage
+	                    :text 0
+	                    :validate :key))
         )
         (declare (ignore mf-exit mf-save mf-load sepm))
-        ; exit key combination
-        ; TODO: fix for Windows
-        (bind *tk* "<Control-q>" (lambda (event) (declare (ignore event)) (setf *exit-mainloop* t)))
-        (wm-title *tk* "Game room")
+        
+        (pack fparams :side :top :pady 10)
+        (configure fparams :borderwidth 5)
+        (configure fparams :relief :sunken)
+        
+            (pack fradio :side :top :fill :x)
+            (pack (list ltoys r1 r2 r3 r4) :side :left)
+            ; set deafult value for toy-type
+            (setf (value r1) "ball")
 
-        (pack fradio :side :top :fill :x)
-        (pack (list ltoys r1 r2 r3 r4) :side :left)
-        ; set deafult value for toy-type
-        (setf (value r1) "ball")
-
+           
+            (pack fname :side :top :fill :x)
+            (pack (list lname i-n) :side :left)
+            (pack fprice :side :top :fill :x)
+            (pack (list lprice i-pf i-pt) :side :left)
+            (pack fage :side :top :fill :x)
+            (pack (list lage i-af i-at) :side :left)
+        
+            ; validate each integer input
+            (format-wish "~A configure -validatecommand {string is int %P}"
+                (widget-path i-pf))
+            (format-wish "~A configure -validatecommand {string is int %P}"
+                (widget-path i-pt))
+            (format-wish "~A configure -validatecommand {string is int %P}"
+                (widget-path i-af))
+            (format-wish "~A configure -validatecommand {string is int %P}"
+                (widget-path i-at))    
+        
+        
+        
         (pack f)
         (configure f :borderwidth 3)
-        (configure f :relief :sunken)
-     
+        (configure f :relief :sunken)     
     )))
 
 (gui)
